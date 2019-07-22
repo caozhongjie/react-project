fe0f3b928395c873de3f421c28978c856e5a0020
caozj@ezgoal.cn <caozj@ezgoal.cn>
2019-07-22 17:07:45 +0800

    '环境搭建'

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.less';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>react</h3>
+    </div>
+  );
+}
+
+export default App;
